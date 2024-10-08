a8133460a54428b8d0124c608d265caa08b1202b
FilderZee <filipveru@gmail.com>
2024-10-08 21:42:48 +0200

    added some more quotes

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,20 @@ const App = () => {
       { quote: "“You only live once, but if you do it right, once is enough.”", quoteauthor: "Mae West" },
       { quote: "“Be the change that you wish to see in the world.”", quoteauthor: "Mahatma Gandhi" },
       { quote: "“In three words I can sum up everything I've learned about life: it goes on.”", quoteauthor: "Robert Frost" },
+      { quote: "“If you tell the truth, you don't have to remember anything.”", quoteauthor: "Mark Twain" },
+      { quote: "“A friend is someone who knows all about you and still loves you.”", quoteauthor: "Elbert Hubbard" },
+      { quote: "“Always forgive your enemies, nothing annoys them so much.”", quoteauthor: "Oscar Wilde" },
+      { quote: "“To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.”", quoteauthor: "Ralph Waldo Emerson" },
+      { quote: "“I have not failed. I've just found 10,000 ways that won't work.”", quoteauthor: "Thomas A. Edison" },
+      { quote: "“It is never too late to be what you might have been.”", quoteauthor: "George Eliot" },
+      { quote: "“Always be a poet, even in prose.”", quoteauthor: "Charles Baudelaire" },
+      { quote: "“I believe I am in Hell, therefore I am.”", quoteauthor: "Arthur Rimbaud" },
+      { quote: "“Either kill me or take me as I am, because I'll be damned if I ever change.”", quoteauthor: "marquis de sade" },
+      { quote: "“The mind is not a vessel to be filled, but a fire to be kindled.”", quoteauthor: "Plutarch" },
+      { quote: "“Be not afraid of greatness. Some are born great, some achieve greatness, and others have greatness thrust upon them.”", quoteauthor: "William Shakespeare" },
+      { quote: "“When one woman strikes at the heart of another, she seldom misses, and the wound is invariably fatal.”", quoteauthor: "Choderlos de Laclos" },
+      { quote: "“It is better to be hated for what you are than to be loved for what you are not.”", quoteauthor: "Andre Gide" },
+      { quote: "“Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring.”", quoteauthor: "Marilyn Monroe" },
     ];
 
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
